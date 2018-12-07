f84079c108a42c020a214d2bab25c182e64a1cfd
AdamJ <adamjames87@gmail.com>
2018-12-07 16:29:25 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,451 @@
+import React, {Component} from 'react';
+import './App.css';
+import {fromJS, getIn, setIn, Map} from "immutable";
+
+function generateUUID() { // Public Domain/MIT
+  var d = new Date().getTime();
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+    d += performance.now(); //use high-precision timer if available
+  }
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+}
+
+// Actions
+const ADD_ROW = 'ADD_ROW';
+const ADD_COMPOSITE_ROW = 'ADD_COMPOSITE_ROW';
+const ADD_COLUMN = 'ADD_COLUMN';
+
+
+// Action Creators
+
+// Add a row after the row listed
+function addRow(afterId) {
+  return {
+    type: ADD_ROW,
+    id: afterId
+  }
+}
+
+// Add a column
+function addColumn(rowId, afterId) {
+  const id = generateUUID();
+  return {
+    type: ADD_COLUMN,
+    rowId: rowId,
+    afterId: afterId,
+    columnId: id
+  }
+}
+
+// State:
+//
+// rows:
+//
+// columns:
+//
+// content:
+//
+
+var originalData = {
+  id: generateUUID(),
+  colSpan: 12,
+  rows: [
+
+    // First row
+    {
+      id: 2,
+      columns: [
+        {
+          // A single column
+          id: generateUUID(),
+          colSpan: 6,
+          rows: [],
+          content: "First Column"
+        },
+
+        {
+          // A single column
+          id: generateUUID(),
+          colSpan: 6,
+          rows: [],
+          content: "Second Column"
+        }
+
+      ]
+    },
+
+    // Second row
+    {
+      id: generateUUID(),
+      columns: [
+        {
+          // A single column
+          id: generateUUID(),
+          colSpan: 6,
+          rows: [],
+          content: "Another Column"
+        },
+
+        {
+          // A single column
+          id: generateUUID(),
+          colSpan: 6,
+          rows: [],
+          content: "and another Column"
+        }
+
+      ]
+    }
+  ]
+};
+
+//
+var originalState = fromJS({
+  entities: {
+    rows: {
+      byId: {
+        "row1" : {
+          id: "row1",
+          columns: ["column1", "column2"]
+        },
+        "row2" : {
+          id: "row2",
+          columns: ["column3", "column4"]
+        }
+      },
+      allIds : ["row1", "row2"]
+    },
+    columns: {
+      byId: {
+        "column1" : {
+          id: "column1",
+          colSpan: 6,
+          content: "First Column"
+        },
+        "column2" : {
+          id: "column2",
+          colspan: 6,
+          content: "Second Column"
+        },
+        "column3" : {
+          id: "column3",
+          colSpan: 4,
+          content: "Third Column"
+        },
+        "column4" : {
+          id: "column4",
+          colSpan: 8,
+          content: "Fourth Column"
+        }
+      },
+      allIds: ["column1", "column2", "column3", "column4"]
+    }
+  }
+});
+
+
+
+// slice reducer
+function columns(state, action) {
+  switch (action.type) {
+    case ADD_COLUMN:
+      const newId = generateUUID();
+      const added = state.setIn(['byId', newId], Map({id: newId, colSpan: 12, content: "I am new"}));
+      const intoList = added.set("allIds", added.get("allIds").push(newId));
+      return intoList;
+    default:
+      return state;
+  }
+}
+
+
+function handleAction(state = originalState, action) {
+  console.log(action)
+  switch (action.type) {
+    case ADD_COLUMN:
+      return  setIn(state, ['entities', 'columns'], columns(getIn(state, ['entities', 'columns']), action));
+  }
+}
+
+
+
+
+// Run a simple test to check I'm sane
+const result =  handleAction( originalState, addColumn(null,null));
+console.log(result);
+
+
+
+// what is the likely structure of a row
+// row {
+// id:
+// columns
+//}
+
+// what is the likely structure of a column
+// col {
+// id:
+// col-span:
+// parent: points to a parent row
+// rows: if rows then show rows
+// content: if content then shows content type
+//}
+
+// What can be reused:
+// A whole row
+// Or the contents of a column
+
+// What does content look like
+// ultimately: a renderable content block =>
+// but usually with metadata
+
+
+// Row expects a row passed in
+class Row extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = props.row;
+  }
+
+
+  renderColumns() {
+    const row = this.props.row;
+    return row.columns.map(
+      col => {
+        return (
+          <Column key={col.id} column={col}/>
+        );
+      });
+  }
+
+  render() {
+    return (
+      <div className="row">
+        {this.renderColumns()}
+      </div>
+    )
+  }
+}
+
+
+class NewRow extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-12 font-sans text-center border-2 border-solid border-black">
+          <div className="p-2">
+            <i className="far fa-plus-square mr-2"></i>
+            Add New Row
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+// Column expects the following props:
+// column
+// isContainer
+class Column extends React.Component {
+
+  static renderRows(column) {
+    if (column.rows == null || column.rows.length == 0) {
+      return (
+        <span>{column.content}</span>
+      );
+
+    } else {
+      return column.rows.map(
+        row => {
+          return (
+            <Row key={row.id} id={row.id} row={row}/>
+          );
+        });
+    }
+  }
+
+  static colClasses() {
+    return "border-2 border-solid border-black p-2"
+  }
+
+  render() {
+    const column = this.props.column;
+    const classNames = Column.colClasses() + " col-md-" + column.colSpan;
+    return (
+      <div className={classNames}>
+        {Column.renderRows(column)}
+      </div>
+    )
+  }
+}
+
+class Grid extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      root: {
+        id: generateUUID(),
+        colSpan: 12,
+        rows: [
+
+          // First row
+          {
+            id: 2,
+            columns: [
+              {
+                // A single column
+                id: generateUUID(),
+                colSpan: 6,
+                rows: [],
+                content: "First Column"
+              },
+
+              {
+                // A single column
+                id: generateUUID(),
+                colSpan: 6,
+                rows: [],
+                content: "Second Column"
+              }
+
+            ]
+          },
+
+          // Second row
+          {
+            id: generateUUID(),
+            columns: [
+              {
+                // A single column
+                id: generateUUID(),
+                colSpan: 6,
+                rows: [],
+                content: "Another Column"
+              },
+
+              {
+                // A single column
+                id: generateUUID(),
+                colSpan: 6,
+                rows: [],
+                content: "and another Column"
+              }
+
+            ]
+          }
+
+        ]
+      }
+    }
+  }
+
+
+  renderRoot() {
+    return Column.renderRows(this.state.root);
+  }
+
+  render() {
+    return (
+      <div>
+        <NewRow/>
+        {this.renderRoot()}
+        <NewRow/>
+      </div>
+    );
+  }
+}
+
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button className="square"
+              onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
+  }
+
+}
+
+class Board extends React.Component {
+
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+
+  renderSquare(i) {
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
+  }
+
+  render() {
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    return (
+      <div>
+        <div className="status">
+          {status}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+
+
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        <Grid/>
+      </div>
+    )
+  }
+}
+
+export default Game;
